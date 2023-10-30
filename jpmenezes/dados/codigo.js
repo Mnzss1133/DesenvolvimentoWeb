e3d15b584b61711827579441756b5e418cce5b40
joao Pedro <joaopedromenezes890@gmail.com>
2023-10-30 15:19:29 -0300

    barra de pesquisa

--- jpmenezes/dados/codigo.js
+++ jpmenezes/dados/codigo.js
@@ -1,19 +1,50 @@
+
+
+const container_filtro = document.createElement('div');
+const texto_pesquisa = document.createElement('input');
+const btn_pesquisa = document.createElement('button');
+
+container_filtro.style.textAlign= 'center';
+btn_pesquisa.innerHTML='Pesquisar';
+btn_pesquisa.onclick= () => {
+    atletas_container.innerHTML='';
+    criar_atletas(filtrar_atletas(texto_pesquisa.value)) ;
+};
+
+container_filtro.appendChild(texto_pesquisa);
+container_filtro.appendChild(btn_pesquisa);
+document.body.appendChild(container_filtro);
+const atletas_container = document.createElement('div');
+document.body.appendChild(atletas_container);
+
+
+
+
+
+
+
 const preenche = (atleta) => {
     const container = document.createElement('div');
     const titulo = document.createElement('h3');
     const imagem = document.createElement('img');
     const descricao = document.createElement('p');
 
+    container.style.width = '15em';
+    container.style.backgroundColor = 'gray';
+    container.style.textAlign = 'center';
+    container.style.margin = 'auto';
+
 
     titulo.innerText =  atleta.nome;
     imagem.src =  atleta.imagem;
+    imagem.alt =`Imagem de ${atleta.nome}`;
     descricao.innerHTML = atleta.descricao;
 
     container.appendChild(titulo);
     container.appendChild(imagem);
     container.appendChild(descricao);
 
-    document.body.appendChild(container);
+   atletas_container.appendChild(container);
 } 
 
 /*
@@ -30,11 +61,30 @@ while (index < atletas.length) {
     index++;
     };
 */
- 
-//let index = 0;
-for(let index = 0;index < atletas.length;index++){
-        preenche(atletas[index]);
-        console.log(index,'loop for');
+ const criar_atletas = (entrada) => {
+    for(let index = 0;index < entrada.length;index++){
+        preenche(entrada[index]);
+
 
     }
-       
+
+ }
+
+ const filtrar_atletas = (texto) => {
+const lista_filtrada = atletas.filter(
+    (e) => {
+        const nome = e.nome
+        const nome_minusculas = nome.toLocaleLowerCase();
+        const texto_minusculas = texto.toLocaleLowerCase();
+        const resultado = nome_minusculas.includes(texto_minusculas);
+        return resultado
+    }
+
+
+    )
+
+    return lista_filtrada;
+ }
+//let index = 0;
+//atletas.filter((e) => e.nome.toLowerCase().includes('y')) Pesquisar todos nomes que possuem y no nome
+criar_atletas(filtrar_atletas('y')) ;      
